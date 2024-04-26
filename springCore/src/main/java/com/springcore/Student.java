7bdc9b7b660ffaaba0a526f41353d7b3c8e1569a
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-04-26 16:10:28 +0530

    first prog in spring

---
+++ springCore/src/main/java/com/springcore/Student.java
@@ -0,0 +1,50 @@
+package com.springcore;
+
+public class Student {
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	
+//	getters and setters
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		System.out.println("studentId setted!");
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
+//	constructor parameterized
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	
+//	default constructor
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+	
+}
