9e414ac228fe9e64183bbcc8389e049f1ab5bd3f
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2024-05-01 19:04:15 +0530

    spring bean core (singleton + prototype)

--- springCore/src/main/java/com/springcore/stereotype/Student.java
+++ springCore/src/main/java/com/springcore/stereotype/Student.java
@@ -1,9 +1,14 @@
 package com.springcore.stereotype;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
+@Component("ob")
+@Scope("prototype") // by default, it's singleton. so it would give same HashCode in that case, but in case of prototype, it would make different objects for different reference variable, thus giving different HashCode for all
 public class Student {
 	
 //	values can be explicitly given using @Value("") annotation
@@ -12,6 +17,12 @@ public class Student {
 	
 	@Value("Delhi")
 	private String city;
+	
+	
+//	here, in the curly braces {}, write the name of id which is written in the config.xml file
+	@Value("#{temp}")
+	private List<String> address;
+	
 	public String getCity() {
 		return city;
 	}
@@ -24,9 +35,19 @@ public class Student {
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
+	
+	
+	
+	public List<String> getAddress() {
+		return address;
+	}
+	public void setAddress(List<String> address) {
+		this.address = address;
+	}
+	
 	@Override
 	public String toString() {
-		return "Student [studentName=" + studentName + ", city=" + city + "]";
+		return "Student [studentName=" + studentName + ", city=" + city + ", address=" + address + "]";
 	}
 	
 	
--- springCore/src/main/java/com/springcore/stereotype/Test.java
+++ springCore/src/main/java/com/springcore/stereotype/Test.java
@@ -8,7 +8,22 @@ public class Test {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/config8.xml");
 		
 //		the name i give below, should be the same name i give in the Component("") {if any given}
-		Student s1 =  context.getBean("student",Student.class);
+//		Student s1 =  context.getBean("student",Student.class);
+		Student s1 =  context.getBean("ob",Student.class);
 		System.out.println(s1);
+		
+		System.out.println(s1.hashCode());
+		
+		Student s2 =  context.getBean("ob",Student.class);
+		System.out.println(s2);
+		
+		System.out.println(s2.hashCode());
+		
+		Teacher t1 = context.getBean("t1", Teacher.class);
+		System.out.println(t1.hashCode());
+		
+		Teacher t2 = context.getBean("t1", Teacher.class);
+		System.out.println(t2.hashCode());
+		
 	}
 }
